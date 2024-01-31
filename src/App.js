4058b88f08bcc8235d04719397e325354aa34a35
Fabian Mejia <Bryan.mejia@solera.com>
2024-01-31 16:34:31 +0100

    chilaquil

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import chilaquil from './images/chilaquil.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={chilaquil} className="App-logo" alt="logo" />
         <p>
           Que oña que oña
         </p>
